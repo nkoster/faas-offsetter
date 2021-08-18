fd2a4a5b1bc7a30952daa82c568098537aba9490
Niels Koster <niels.koster@portavita.nl>
2021-08-18 10:34:22 +0200

    ATM-2771: Initial, Offsetter API as a function

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = async (body, res) => {
+
+  const {topic, partition, offset} = body
+  const broker = 'pvdevkafka01'
+
+  console.log(topic, partition, offset)
+
+  const { exec } = require('child_process')
+  const execute = (cmd, callback) => exec(cmd, (_, stdout) => callback(stdout))
+  
+  const data = await new Promise((resolve, reject) => {
+    try {
+      const cmd = `kafkacat -C -b ${broker} -t ${topic} -p ${partition} -o ${offset} -c 1 -e -q`
+      execute(cmd, result => resolve(result))
+      console.log(cmd)
+    } catch (err) {
+      console.log(err.message)
+      reject({})
+    }
+  })
+
+  return res.status(200).send(JSON.parse(data))
+    
+}
